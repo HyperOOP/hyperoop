0bb5a9844c1ed36583574c2141a3cd14cfb2e4f1
Osman Bineev <bineev@gmail.com>
2018-11-08 19:43:19 +0300

    building hyperoop.js.gz added

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,13 @@
 import typescript from 'rollup-plugin-typescript2';
 import resolve    from 'rollup-plugin-node-resolve';
+import gzip       from 'rollup-plugin-gzip';
  
 export default {
     plugins: [
         typescript({
             typescript: require('typescript'),
         }),
-        resolve()
+        resolve(),
+        gzip()
     ]
 }
