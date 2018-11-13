a13b39f36ca86b37302bd128783243d4f6cfd407
Osman Bineev <bineev@gmail.com>
2018-11-13 20:58:02 +0300

    linter added

--- src/hyperoop.ts
+++ src/hyperoop.ts
@@ -1,15 +1,17 @@
-import * as hyperapp  from 'hyperapp';
-import * as proxperty from './proxperty';
+import * as hyperapp from "hyperapp";
+import * as proxperty from "./proxperty";
 
-import Hist from 'redoundo';
+import Hist from "redoundo";
 
-type Spin = {
+/* tslint:disable:typedef-whitespace max-classes-per-file forin*/
+
+interface ISpin {
     Value: boolean;
 }
 
 /** Type of renderer that should be called for page re-rendering. */
-export type Renderer = {
-    render: ()=>(spin: Spin)=>Spin;
+export interface IRenderer {
+    render: () => (spin: ISpin) => ISpin;
 }
 
 /** JSX factory function, creates `VNode`s */
@@ -19,127 +21,127 @@ export let h = hyperapp.h;
 export type VNode<A> = hyperapp.VNode<A>;
 
 /** The view function describes the application UI as a tree of VNodes. */
-export type View = hyperapp.View<Spin, Renderer>;
+export type View = hyperapp.View<ISpin, IRenderer>;
 
 /** Creates `View` object
- * 
+ *
  * @param a `Actions` object
  * @param v function that returns a VDOM tree
  */
-export function view<S extends object, A extends Actions<S>>(a: A, v: ()=>VNode<object>): View {
-    return (spin, renderer) => {
-        if (a) a.init(renderer);
+export function view<S extends object, A extends Actions<S>>(a: A, v: () => VNode<object>): View {
+    return (spin, r) => {
+        if (a) { a.init(r); }
         return v();
-    }
+    };
 }
 
-let renderer: Renderer = { render:() => s => ({Value: !s.Value}) };
+const renderer: IRenderer = { render: () => (s) => ({Value: !s.Value}) };
 
 /** initialize DOM element with a hyperoop `View`
- * 
- * @param el 
- * @param view 
+ *
+ * @param el
+ * @param view
  */
-export function init(el: HTMLElement, view: View) {
-    hyperapp.app({Value: true}, renderer, view, el)
+export function init(el: HTMLElement, v: View) {
+    hyperapp.app({Value: true}, renderer, v, el);
 }
 
 /** Interface of a parental `Actions` */
-export interface ActionsParentI {
+export interface IActionsParent {
     /** renderer that should be called for page re-rendering */
-    readonly Renderer: Renderer ;
+    readonly Renderer: IRenderer ;
     /** `redoundo.Hist` object for redo/undo functionality */
-    readonly History:  Hist;
+    readonly History: Hist;
 }
 
 /** Class of hyperoop top-level action */
 export class Actions<S extends object> {
-    private orig_:     S;
-    private state_:    S;
-    private remember_: S;
-    private renderer_: Renderer;
-
     /** state object */
-    get State():    S { return this.state_ }
+    get State(): S { return this.state; }
     /** state object that remember previous states and has redo/undo functionality */
-    get Remember(): S { return this.remember_ }
+    get Remember(): S { return this.remember; }
     /** renderer that should be called for page re-rendering */
-    get Renderer(): Renderer { return this.renderer_ }
-    
+    get Renderer(): IRenderer { return this.renderer; }
+
     /** `redoundo.Hist` object implements redo/undo functionality */
-    readonly History:  Hist;
+    public readonly History: Hist;
+
+    private orig:     S;
+    private state:    S;
+    private remember: S;
+    private renderer: IRenderer;
 
     /** Construct an `Actions` object
-     * 
+     *
      * @param start state on start
      * @param hist `redoundo.Hist` object implements redo/undo functionality
      */
     constructor(start: S, hist: number | Hist = null) {
-        this.orig_     = start;
-        this.History   = typeof hist === 'number' ? new Hist(hist) : hist;
+        this.orig     = start;
+        this.History   = typeof hist === "number" ? new Hist(hist) : hist;
         this.init(renderer);
     }
 
     /** Partially sets a new state
-     * 
+     *
      * @param s new state data
      * @param remember remember previous state using `redoundo.Hist` or not?
      */
-    set(s: Partial<S>, remember: boolean = false) {
-        let keys = Object.getOwnPropertyNames(s).filter(
-            k => !(Array.isArray(s) && k === 'length') && !((k in this.orig_) && this.orig_[k] === s[k])
+    public set(s: Partial<S>, remember: boolean = false) {
+        const keys = Object.getOwnPropertyNames(s).filter(
+            (k) => !(Array.isArray(s) && k === "length") && !((k in this.orig) && this.orig[k] === s[k]),
         );
-        let change = keys.length > 0;
-        if (!change) return;
-        let self = this;
+        const change = keys.length > 0;
+        if (!change) { return; }
+        const self = this;
         if (remember && this.History) {
-            let was: Partial<S> = {};
-            let wasnt: string[] = [];
-            for (let k of keys) {
-                if (k in this.orig_) was[k] = this.orig_[k];
-                else wasnt.push(k);
+            const was: Partial<S> = {};
+            const wasnt: string[] = [];
+            for (const k of keys) {
+                if (k in this.orig) {
+                    was[k] = this.orig[k];
+                } else { wasnt.push(k); }
             }
             this.History.add({
-                Redo: ()=>{
-                    for (let k of keys) self.orig_[k] = s[k];
-                    self.renderer_.render();
+                Redo: () => {
+                    for (const k of keys) { self.orig[k] = s[k]; }
+                    self.renderer.render();
                 },
-                Undo: ()=>{
-                    for (let k in was) self.orig_[k] = was[k];
-                    for (let k of wasnt) delete self.orig_[k];
-                    self.renderer_.render();
-                }
-            })
-        }
-        else {
-            for (let k of keys) this.orig_[k] = s[k];
-            this.renderer_.render();
+                Undo: () => {
+                    for (const k in was) { self.orig[k] = was[k]; }
+                    for (const k of wasnt) { delete self.orig[k]; }
+                    self.renderer.render();
+                },
+            });
+        } else {
+            for (const k of keys) { this.orig[k] = s[k]; }
+            this.renderer.render();
         }
     }
 
     /** Initialize `Actions` with new renderer
-     * 
-     * @param r 
+     *
+     * @param r
      */
-    init(r: Renderer) {
-        this.renderer_ = r;
-        let self = this;
-        this.state_    = proxperty.make(this.orig_, ()=>self.renderer_.render());
-        this.remember_ = proxperty.makeH(this.orig_, ()=>self.renderer_.render(), this.History);
+    public init(r: IRenderer) {
+        this.renderer = r;
+        const self    = this;
+        this.state    = proxperty.make(this.orig, () => self.renderer.render());
+        this.remember = proxperty.makeH(this.orig, () => self.renderer.render(), this.History);
     }
 }
 
 /** Class of hyperoop sub-actions */
 export class SubActions<S extends object> extends Actions<S> {
-    
+
     /** Constructs `SubActions` object inheriting `History` and `Renderer` from a parent.
-     *  NOTE! If `SubActions` object created before first rendering then you will need 
+     *  NOTE! If `SubActions` object created before first rendering then you will need
      *  to call it's `init` manually.
-     * 
+     *
      * @param start state on start
      * @param parent parent `(Sub)Actions` object.
      */
-    constructor(start: S, parent: ActionsParentI) {
+    constructor(start: S, parent: IActionsParent) {
         super(start, parent.History);
         if (parent.Renderer) {
             this.init({render: () => parent.Renderer.render()});
--- src/proxperty.ts
+++ src/proxperty.ts
@@ -1,75 +1,76 @@
-import Hist from 'redoundo';
+import Hist from "redoundo";
 
 /**
  * Creates Proxy that calls `after` callback after set or delete entries of a `target`.
- * 
- * 
+ *
+ *
  * @param target target object
  * @param after callback to execute after set or delete entries of `target`
  */
-export function make<T extends object>(target: T, after: ()=>void): T {
+export function make<T extends object>(target: T, after: () => void): T {
     return new Proxy<T>(target, {
-        set: (target, k, v) => {
-            if (k in target && target[k] === v) return true;
-            target[k] = v;
+        set: (t, k, v) => {
+            if (k in t && t[k] === v) { return true; }
+            t[k] = v;
             after();
             return true;
         },
 
-        deleteProperty: (target, k) => {
-            if (k in target) {
-                delete target[k];
+        deleteProperty: (t, k) => {
+            if (k in t) {
+                delete t[k];
                 after();
             }
             return true;
-        }
+        },
     });
 }
 
 /**
  * Creates Proxy that calls `after` callback after set or delete entries of a `target`.
  * Set or delete actions can be (re, un)done using `redoundo.Hist` argument.
- * 
+ *
  * @param target target object
  * @param after callback to execute after set or delete entries of `target`
  * @param hist `redoundo.Hist` object
  */
-export function makeH<T extends object>(target: T, after: ()=>void, hist: Hist): T {
-    if (!hist) return null;
+export function makeH<T extends object>(target: T, after: () => void, hist: Hist): T {
+    if (!hist) { return null; }
     return new Proxy<T>(target, {
-        set: (target, k, v) => {
-            let was = k in target;
+        set: (t, k, v) => {
+            const was = k in target;
             let oldVal = null;
             if (was) {
                 oldVal = target[k];
-                if (oldVal === v) return true;
+                if (oldVal === v) { return true; }
             }
-            let redo = ()=>{
+            const redo = () => {
                 target[k] = v;
                 after();
             };
-            let undo = ()=>{
-                if (was) target[k] = oldVal;
-                else delete target[k];
+            const undo = () => {
+                if (was) {
+                    target[k] = oldVal;
+                } else { delete target[k]; }
                 after();
             };
-            hist.add({ Redo: redo, Undo: undo })
+            hist.add({ Redo: redo, Undo: undo });
             return true;
         },
 
-        deleteProperty: (target, k) => {
-            if (!(k in target)) return true;
-            let v = target[k];
-            let redo = ()=>{
-                delete target[k];
+        deleteProperty: (t, k) => {
+            if (!(k in t)) { return true; }
+            const v = t[k];
+            const redo = () => {
+                delete t[k];
                 after();
             };
-            let undo = ()=>{
-                target[k] = v;
+            const undo = () => {
+                t[k] = v;
                 after();
             };
-            hist.add({ Redo: redo, Undo: undo })
+            hist.add({ Redo: redo, Undo: undo });
             return true;
-        }
+        },
     });
 }
