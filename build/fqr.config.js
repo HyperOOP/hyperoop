dd29de89d6d4bbf08befad38c44109c14a648ba1
Osman Bineev <bineev@gmail.com>
2019-01-03 00:16:55 +0300

    build process faqtorized

--- build/fqr.config.js
+++ build/fqr.config.js
@@ -4,7 +4,7 @@ const { roll } = require("faqtor-of-rollup");
 const { minify } = require("faqtor-of-uglify");
 const { compress } = require("faqtor-of-gzip");
 const { lock, publish } = require("faqtor-of-publish");
-const { seq } = require("faqtor");
+const { seq, cmd } = require("faqtor");
 1
 const
     input = "src/**/*",
@@ -13,7 +13,11 @@ const
     hyperoopUmd = `dist/${hyperoop}.umd.js`,
     hyperoopMin = `dist/${hyperoop}.min.js`,
     hyperoopEs = `dist/${hyperoop}.es.js`;
-    pubArgs = ["dist", "build/dist-lock.json"];
+    pubArgs = ["dist", "build/dist-lock.json"],
+    tsconfigTest = "build/tsconfig.test.json",
+    testDist = "test/dist",
+    toClean = ["dist", "test/dist"],
+    toWipe = toClean.concat(["./node_modules", "./coverage", "./.rpt2_cache"]);
 
 const tsconfigBuild = {
     include: [
@@ -52,18 +56,24 @@ const rollupUmdCfg = {
     plugins: rollupPlugins
 }
 
-const jestConfig = JSON.stringify({
-    testEnvironment: 'node',
-});
+const tsc = (project) => cmd(`tsc -p ${project}`)
 
 const
     buildEs = roll(rollupEsCfg).factor(input, hyperoopEs),
     buildUmd = roll(rollupUmdCfg).factor(input, hyperoopUmd),
     uglify = minify(hyperoopUmd, hyperoopMin).factor(),
     gzip = compress(hyperoopMin, `${hyperoopMin}.gz`).factor(),
-    locker = lock(...pubArgs).factor();
+    locker = lock(...pubArgs).factor(),
+    buildTest = tsc(tsconfigTest),
+    jest = cmd(`jest --coverage --env=jsdom`),
+    testTs = tsc("test/ts"),
+    clean = cmd(`rimraf ${toClean.join(" ")}`),
+    wipe = cmd(`rimraf ${toWipe.join(" ")}`);
 
 module.exports = {
     build: seq(buildEs, buildUmd, uglify, gzip, locker),
-    publish: publish(...pubArgs)
+    test: seq(buildTest, jest, testTs),
+    publish: publish(...pubArgs),
+    clean,
+    wipe
 }
